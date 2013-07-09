2be1d053c1ac5ece7d4a3606f0c2bb276afc4f4c
Eric Steele <eric@notvelcro.com>
2013-07-09 10:48:57 -0400

    fixed task name for omniauth

--- recipes/omniauth.rb
+++ recipes/omniauth.rb
@@ -1,6 +1,6 @@
 namespace :omniauth do
-  desc "Setup omni lib dependencies"
-  task :setup, roles: :app do
+  desc "Install omni lib dependencies"
+  task :install, roles: :app do
     run "#{sudo} apt-get update"
     run "#{sudo} apt-get install -y libxslt-dev libxml2-dev"
   end
